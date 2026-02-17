a9053b365e5044545a3368773264f61a3d0a1553
kimseonguk197 <ksg39412@naver.com>
2026-02-17 14:45:49 +0900

    build.gradle 수정

--- consumer1/src/main/java/com/example/consumer/service/실습4.java
+++ consumer1/src/main/java/com/example/consumer/service/실습4.java
@@ -1,38 +1,40 @@
-//package com.example.consumer.service;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.kafka.support.Acknowledgment;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//@Transactional
-//public class 실습4 {
-//    private final ObjectMapper objectMapper;
-//    public 실습4(ObjectMapper objectMapper) {
-//        this.objectMapper = objectMapper;
-//    }
-////    테스트1. 실패된 메시지 재처리
-//    @KafkaListener(
-//            topics = "member-topic",
-//            groupId = "${spring.kafka.consumer.member-topic-log-group-id}",
-//            containerFactory = "kafkaListener"
-//    )
-//    public void consumer1(
-//            String message
-//            , Acknowledgment ack
-//    ) {
-//        try {
-//            System.out.println("컨슈머 메시지 수신1 : " + message);
-//////            db작업 및 실패가능성 있는 코드
-////            if(true){
-////                throw new IllegalArgumentException("예상치 못한 예외 발생");
-////            }
-//            ack.acknowledge(); //수동 커밋
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//    }
-//
-//}
+package com.example.consumer.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class 실습4 {
+    private final ObjectMapper objectMapper;
+    public 실습4(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+//    테스트1. 실패된 메시지 재처리
+    @KafkaListener(
+            topics = "member-topic",
+            groupId = "${spring.kafka.consumer.member-topic-log-group-id}",
+            containerFactory = "kafkaListener"
+    )
+    public void consumer1(
+            String message
+            , Acknowledgment ack
+    ) {
+        try {
+            System.out.println("컨슈머 메시지 수신1 : " + message);
+            Thread.sleep(120_000); // 2분 (120초)
+////            db작업 및 실패가능성 있는 코드
+//            if(true){
+//                throw new IllegalArgumentException("예상치 못한 예외 발생");
+//            }
+            ack.acknowledge(); //수동 커밋
+            System.out.println("처리 완료");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
