f370099be72a91382317b70347b4621303f366eb
kimseonguk <71623837+kimseonguk197@users.noreply.github.com>
2026-02-18 22:28:38 +0900

    Update 실습2.java

--- consumer1/src/main/java/com/example/consumer/service/실습2.java
+++ consumer1/src/main/java/com/example/consumer/service/실습2.java
@@ -16,6 +16,7 @@
 //
 ////  테스트1 : 2대의 컨슈머서버가 같은 그룹ID를 가지고 같은 topic을 listen 경우
 ////  테스트2 : 2대의 컨슈머서버가 다른 그룹ID를 가지고 같은 topic을 listen 경우
+//    테스트3 : 컨슈머그룹ID가 다른 서버에서 auto-offset-reset : earliest 또는 latest 설정
 //    @KafkaListener(
 //            topics = "member-topic",
 //            groupId = "${spring.kafka.consumer.member-topic-log-group-id}",
