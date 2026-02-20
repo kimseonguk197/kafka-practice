a6c2c8921b5d9d104a942d188c64796b005dc46b
kimseonguk <71623837+kimseonguk197@users.noreply.github.com>
2026-02-20 19:12:55 +0900

    Update 실습4.java

--- consumer1/src/main/java/com/example/consumer/service/실습4.java
+++ consumer1/src/main/java/com/example/consumer/service/실습4.java
@@ -9,11 +9,9 @@
 //@Service
 //@Transactional
 //public class 실습4 {
-//    private final ObjectMapper objectMapper;
-//    public 실습4(ObjectMapper objectMapper) {
-//        this.objectMapper = objectMapper;
-//    }
+//
 ////    테스트1. 실패된 메시지 재처리
+////    기본 모드 : auto-commit
 //    @KafkaListener(
 //            topics = "member-topic",
 //            groupId = "${spring.kafka.consumer.member-topic-log-group-id}",
@@ -21,20 +19,37 @@
 //    )
 //    public void consumer1(
 //            String message
-//            , Acknowledgment ack
 //    ) {
 //        try {
 //            System.out.println("컨슈머 메시지 수신1 : " + message);
-//            Thread.sleep(120_000); // 2분 (120초)
-//////            db작업 및 실패가능성 있는 코드
-////            if(true){
-////                throw new IllegalArgumentException("예상치 못한 예외 발생");
-////            }
-//            ack.acknowledge(); //수동 커밋
+////            db작업 및 실패가능성 있는 코드
+//            if(true){
+//                throw new IllegalArgumentException("예상치 못한 예외 발생");
+//            }
 //            System.out.println("처리 완료");
 //        }catch (Exception e){
 //            e.printStackTrace();
 //        }
 //    }
+//
+//////    수동 커밋 모드로 변경
+////    @KafkaListener(
+////            topics = "member-topic",
+////            groupId = "${spring.kafka.consumer.member-topic-log-group-id}",
+////            containerFactory = "kafkaListener"
+////    )
+////    public void consumer1(
+////            String message
+////            , Acknowledgment ack
+////    ) {
+////
+////        System.out.println("컨슈머 메시지 수신1 : " + message);
+//////            db작업 및 실패가능성 있는 코드
+////        if(true){
+////            throw new IllegalArgumentException("예상치 못한 예외 발생");
+////        }
+////        ack.acknowledge(); //수동 커밋
+////        System.out.println("처리 완료");
+////    }
 //
 //}
